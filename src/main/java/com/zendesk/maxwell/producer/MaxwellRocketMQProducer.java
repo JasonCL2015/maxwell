0d66e796da2a4a8bee5190e7e5b84edf49399043
pufang <pufang@maihaoche.com>
2017-08-07 22:33:18 +0800

    cache produce exception

--- src/main/java/com/zendesk/maxwell/producer/MaxwellRocketMQProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/MaxwellRocketMQProducer.java
@@ -16,6 +16,7 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -184,7 +185,11 @@ class MaxwellRocketMQProducerWorker extends AbstractAsyncProducer implements Run
 		RocketMQCallback callback = new RocketMQCallback(cc, r.getPosition(), key, value, this.metricsTimer,
 				this.succeededMessageCount, this.failedMessageCount, this.succeededMessageMeter, this.failedMessageMeter, this.context);
 
-		rocketmq.send(message, callback);
+		try {
+			rocketmq.send(message, callback);
+		} catch (Exception e) {
+			LOGGER.error("send message error", e.getMessage());
+		}
 	}
 
 	@Override
